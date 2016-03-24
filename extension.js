799666311c53191ece5f26e01a36b8e4cc4a54e9
William Wong <compulim@hotmail.com>
2016-03-25 03:05:13 +0800

    Add keywords

--- extension.js
+++ extension.js
@@ -2,12 +2,11 @@
 
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-var vscode = require('vscode');
+const vscode = require('vscode');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   // console.log('Congratulations, your extension "4to2" is now active!');
@@ -15,7 +14,7 @@ function activate(context) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  var disposable = vscode.commands.registerTextEditorCommand('4to2.convert4to2', function (textEditor, edit) {
+  context.subscriptions.push(vscode.commands.registerTextEditorCommand('4to2.convert4to2', function (textEditor, edit) {
     // The code you place here will be executed every time your command is executed
 
     // Display a message box to the user
@@ -31,9 +30,7 @@ function activate(context) {
         convertIndent4to2(line)
       );
     }
-  });
-
-  context.subscriptions.push(disposable);
+  }));
 }
 
 exports.activate = activate;
